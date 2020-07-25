d71424f67d1c4434d6d70d5401f399a6c0820850
VikasBelida09 <62166406+VikasBelida09@users.noreply.github.com>
2020-07-25 22:12:11 +0530

    Update Charts.jsx

--- src/Components/Charts/Charts.jsx
+++ src/Components/Charts/Charts.jsx
@@ -10,7 +10,7 @@ export default function Charts({data:{confirmed,deaths,recovered},country}) {
         }
         fetchAPI()
         console.log(dailydata)
-    }, [])
+    }, [dailydata])
 
     const lineChart=()=>{
       return (
